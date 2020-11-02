621b164b888c1b4e509f236fbe5f808435ed9b18
birkin <birkin.diana@gmail.com>
2020-11-02 06:46:17 -0500

    creates manually handled route.

--- hello-world-app/src/main.rs
+++ hello-world-app/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+
+use actix_web::{ web, App, HttpServer, Responder };
+
+
+async fn index() -> impl Responder {
+    "Hello world from the app example!"
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result< () > {
+    HttpServer::new( || {
+        App::new().service(
+            // -- prefixes all resources nd routes attached to it...
+            web::scope( "/app" )
+                // -- ...so this handles requests for `GET /app/index.html`
+                .route( "/index.html", web::get().to(index) ),
+        )
+    } )
+    .bind( "127.0.0.1:8081" )?
+    .run()
+    .await  // now accessible via `http://127.0.0.1:8081/app/index.html`
 }
